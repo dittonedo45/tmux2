ed467e59e752919b881ab9a5ee52e721c0c89b92
Eddington Chamunorwa <dittonedo45@github.com>
2022-03-08 08:59:54 +0200

    lua: better off.

--- cmd-lua.c
+++ cmd-lua.c
@@ -1,4 +1,4 @@
-/*XXX This Document was modified on 1646721337 */
+/*XXX This Document was modified on 1646722243 */
 #include <tmux.h>
 #include <lua.h>
 #include <lauxlib.h>
@@ -18,12 +18,29 @@ static int tl_print ( lua_State * s )
 {
  const char *str = lua_tostring ( s, -1 );
  if( str )
-  cmdq_print ( lua_item, str );
+  cmdq_print ( lua_item, "%s", str );
  return 0;
 }
 
 int cmd_lua_exec ( struct cmd *self, struct cmdq_item *item )
 {
  lua_item = item;
- return 0;
+ struct args *args = cmd_get_args ( self );
+
+ lua_State *s = luaL_newstate (  );
+ int ret = CMD_RETURN_NORMAL;
+ do {
+  lua_pushcfunction ( s, tl_print );
+  lua_setglobal ( s, "print" );
+
+  luaL_loadstring ( s, args->argv[0] );
+  lua_pcall ( s, 0, 0, 0 );
+  if( lua_gettop ( s ) ) {
+   cmdq_error ( item, "%s", lua_tostring ( s, -1 ) );
+   ret = CMD_RETURN_ERROR;
+  }
+ } while( 0 );
+
+ lua_close ( s );
+ return ret;
 }
