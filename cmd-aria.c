19d67fcbc2542b1ae5349b11e0b651efd8421b30
Eddington Chamunorwa <dittonedo45@github.com>
2022-03-11 10:05:38 +0200

    I love lisp.

--- cmd-aria.c
+++ cmd-aria.c
@@ -1,4 +1,4 @@
-/*XXX This Document was modified on 1646982400 */
+/*XXX This Document was modified on 1646985929 */
 #include <tmux.h>
 #include <string.h>
 
@@ -14,17 +14,6 @@ const struct cmd_entry cmd_lua_entry = {
  .flags = CMD_STARTSERVER,
  .exec = cmd_aria_exec
 };
-
-ar_Value *traceback ( ar_State * S, ar_Frame * until )
-{
- ar_Value *res = NULL, **last = &res;
- ar_Frame *f = S->frame;
- while( f != until ) {
-  last = ar_append_tail ( S, last, f->caller );
-  f = f->parent;
- }
- return res;
-}
 
 ar_Value *tl_pr_func ( ar_State * s, ar_Value * args )
 {
@@ -42,13 +31,47 @@ ar_Value *tl_pr_func ( ar_State * s, ar_Value * args )
  return ar_car ( p );
 }
 
+ar_Value *traceback ( ar_State * s, ar_Frame * until )
+{
+ ar_Value *res = NULL, **last = &res;
+ ar_Frame *f = s->frame;
+ while( f != until ) {
+  last = ar_append_tail ( s, last, f->caller );
+  f = f->parent;
+ }
+ return res;
+}
+
+static ar_Value *char_ss_2 ( ar_State * s, char **v )
+{
+
+ ar_Value *arg = 0;
+ ar_Value **lp = &arg;
+#define arg_add(x) lp = ar_append_tail ( s, lp, x )
+ while( v && *v ) {
+  arg_add ( ar_new_string ( s, *v ) );
+  v++;
+ }
+ return arg;
+}
+
 static
 ar_Value *tl_TOF_func ( ar_State * s, ar_Value * args )
 {
  const struct options_table_entry *p = options_table;
 
  while( p && p->name ) {
-  ar_Value *arg = ar_new_string ( s, p->name );
+  ar_Value *arg = ar_new_list ( s, 9,
+                                ( ar_new_string ( s, p->name ) ),
+                                ( ar_new_number ( s, p->type ) ),
+                                ( ar_new_number ( s, p->minimum ) ),
+                                ( ar_new_number ( s, p->maximum ) ),
+                                ( ar_new_number ( s, p->type ) ),
+                                ( char_ss_2 ( s, p->choices ) ),
+                                ( ar_new_string ( s, p->default_str ) ),
+                                ( ar_new_number ( s, p->default_num ) ),
+                                ( char_ss_2 ( s, p->default_arr ) )
+       );
 
   ar_Value *fun = ar_car ( args );
   if( !fun || !( ar_type ( fun ) == AR_TFUNC || ar_type ( fun ) == AR_TCFUNC ) )
@@ -91,10 +114,8 @@ int cmd_aria_exec ( struct cmd *self, struct cmdq_item *item )
   if( !lisp_s )
    break;
   do {
-   ar_bind_global ( lisp_s, "tmux.options.foreach",
-                    ar_new_cfunc ( lisp_s, tl_TOF_func ) );
-   ar_bind_global ( lisp_s, "tmux.display",
-                    ar_new_cfunc ( lisp_s, tl_pr_func ) );
+   ar_bind_global ( lisp_s, "t.o.f", ar_new_cfunc ( lisp_s, tl_TOF_func ) );
+   ar_bind_global ( lisp_s, "t.p", ar_new_cfunc ( lisp_s, tl_pr_func ) );
 
    if( args->argc && lisp_s ) {
 	char *fi_or_source = args->argv[0];
